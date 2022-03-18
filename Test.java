5ed7cf15c00119218ab81f811726b6fff84181e8
julHristov <101811818+julHristov@users.noreply.github.com>
2022-03-18 11:04:03 +0200

    HomeWork1

---
+++ Test.java
@@ -0,0 +1,11 @@
+import java.util.Locale;
+
+public class Test {
+
+    public static void main(String[] args) {
+        String myName = "Yulian";
+        System.out.println(myName.length());
+        System.out.println(myName.toUpperCase());
+        System.out.println(myName.toLowerCase());
+    }
+}
